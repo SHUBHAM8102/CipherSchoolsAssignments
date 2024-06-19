b4143dea9d33f756dbfce574f175ec4722a08294
Shubham Kumar <115077964+SHUBHAM8102@users.noreply.github.com>
2024-06-19 23:51:04 +0530

    Update Lecture5(1).cpp

---
+++ cipherschools/Course 01/Lecture5(1).cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a,b;
+    int c,d;
+
+    cout<<"Input 2 integers a and b"<<endl;
+
+    cin>>a;
+    cin>>b;
+
+    cout<<"Input 2 integers c and d"<<endl;
+    cin>>c;
+    cin>>d;
+
+
+    cout<<(a>b && c>d);
+    cout<<(a>b && c>d);
+
+
+
+
+    return 0;
+}
