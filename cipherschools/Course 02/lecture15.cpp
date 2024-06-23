8013b8704e97684de79e4d61ceae8384e5b42f29
Shubham Kumar <115077964+SHUBHAM8102@users.noreply.github.com>
2024-06-23 20:29:35 +0530

    Update lecture15.cpp

--- cipherschools/Course 02/lecture15.cpp
+++ cipherschools/Course 02/lecture15.cpp
@@ -1,1 +1,37 @@
+// To compare two numbers and the greatest integres 
+#include<iostream>
+using namespace std;
+int compare(int a, int b)
+{
+    if(a>b)
+    {
+        return a;
+    }
+    else{
+        return b;
+    }
+}
 
+
+
+
+//Addition of two numbers.
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+    int c;
+    c=a+b;
+    return c;
+}
+int main()
+{
+    int a,b;
+    int add_sum;
+    a=3;
+    b=5;
+    add_sum=sum(a,b);
+    cout<<add_sum;
+    return 0;
+
+}
