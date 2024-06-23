041d20ebd981609d92c5b05cb843e17203221509
Shubham Kumar <115077964+SHUBHAM8102@users.noreply.github.com>
2024-06-23 23:35:19 +0530

    Update lecture17.cpp

--- cipherschools/Course 02/lecture17.cpp
+++ cipherschools/Course 02/lecture17.cpp
@@ -1,1 +1,60 @@
+//function exapmle
 
+#include<iostream>
+using namespace std;
+void PrintHi()
+{
+    cout<<"Hi"<<endl;
+}
+int sum(int a,int b)
+{
+    int c;
+    c=a+b;
+    return c;
+}
+int main()
+{
+    PrintHi();
+    int a,b;
+    a=4;
+    b=6;
+    int d;
+    d=sum(a,b);
+    cout<<"The sum is egual to "<<d;
+    return 0;
+}
+
+
+
+
+
+
+//function(local variable)
+
+#include<iostream>
+using namespace std;
+void PrintHi()
+{
+    cout<<"Hi"<<endl;
+}
+int sum(int a,int b)
+{
+    int c;
+    c=a+b;
+    a=500;
+    cout<<"The value of c "<<c<<endl;
+    return c;
+
+}
+int main()
+{
+    PrintHi();
+    int a,b,c;
+    a=4;
+    b=6;
+    c=75
+    int d;
+    d=sum(a,b);
+    cout<<"The value of c "<<c<<endl;
+    return 0;
+}
