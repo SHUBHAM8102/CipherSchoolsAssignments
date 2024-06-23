3cce1995020e3867f0296d41ac018047583dd429
Shubham Kumar <115077964+SHUBHAM8102@users.noreply.github.com>
2024-06-23 21:26:12 +0530

    Update lecture16.cpp

--- cipherschools/Course 02/lecture16.cpp
+++ cipherschools/Course 02/lecture16.cpp
@@ -1,1 +1,51 @@
+//function
 
+#include<iostream>
+using namespace std;
+int sum(int a,int b)
+{
+    int c;
+     a=10;
+     b=11;
+    c=a+b;
+    return c;
+}
+int main()
+{
+    int a,b;
+    a=3;
+    b=5;
+    int c;
+    cout<<a<<" "<<b<<endl;
+    c=sum(a,b);
+    cout<<c<<endl;
+    cout<<a<<" "<<b<<endl;
+    return 0;
+
+}
+
+
+
+
+//function prototype
+
+#include<iostream>
+using namespace std;
+int sum(int ,int )
+int main()
+{
+    int a,b;
+    a=3;
+    b=5;
+    int c;
+    cout<<a<<" "<<b<<endl;
+    c=sum(a,b);
+    cout<<c<<endl;
+    cout<<a<<" "<<b<<endl;
+    return 0;
+
+}
+int sum(int a,int b)
+{
+    return a+b;
+}
