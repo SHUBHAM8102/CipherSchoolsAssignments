b77b9855163bf8b215405d4e072e1c2522157252
Shubham Kumar <115077964+SHUBHAM8102@users.noreply.github.com>
2024-06-24 23:22:37 +0530

    Update lecture19.cpp

--- cipherschools/Course 02/lecture19.cpp
+++ cipherschools/Course 02/lecture19.cpp
@@ -1,1 +1,9 @@
-
+//memory
+#include<iostream>
+using namespace std;
+int main(){
+    long long int a;
+    int b;
+    cout<<"The size of int is: "<<sizeof(b)<<" The size of long long int is: "<<sizeof(a);
+    return 0;
+}
