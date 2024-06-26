9e71c209a7fd1c3945fe393158c6cb5028657aa9
Shubham Kumar <115077964+SHUBHAM8102@users.noreply.github.com>
2024-06-26 23:49:28 +0530

    Update lecture23.cpp

--- cipherschools/Course 02/lecture23.cpp
+++ cipherschools/Course 02/lecture23.cpp
@@ -1,1 +1,36 @@
+//ScalerFactor
+
+#include<iostream>
+using namespace std;
+int main(){
+    int a=5;
+    int *x;
+    x=&a;
+    cout<<"The address of a,which is store in the printer x is: "<<x<<" The value of x+1 is: "<<x+1<<endl;
+    cout<<x+2;
+    return 0;
+}
+
+
+
+
+
+
+
+//ScalerFactor2
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a[5];
+    int *x;
+    x=&a[0];
+
+    int i;
+    for(i=0;i<6;i++){
+        cout<<&a[i]<<endl;
+        }
+    return 0;
+}
 
