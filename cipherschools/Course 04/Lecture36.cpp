1fa9a4c938920064886ddc1bb0a18d2888a4477c
Shubham Kumar <115077964+SHUBHAM8102@users.noreply.github.com>
2024-07-09 23:18:26 +0530

    Create Lecture36.cpp

---
+++ cipherschools/Course 04/Lecture36.cpp
@@ -0,0 +1,13 @@
+//Algorithms
+
+1. What are Algoritms?
+ans. Algorithms are solution to a problem.
+
+Different kinds of Algoritms
+->Sorting
+->Searching
+->Recursion
+->Greedy
+->Dynamic programming
+->Graph Algorithms
+->Back-tracking Algorithms
